29461fd0ab1e63ed1fc5280fcd319e444b397c93
Aleksey Covacevice <aleksey.covacevice@gmail.com>
2018-04-17 21:02:34 -0300

    Add initial version
    This commit adds the initial version of the tool.

---
+++ btckey.c
@@ -0,0 +1,169 @@
+#define _GNU_SOURCE
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <openssl/ec.h>
+#include <openssl/evp.h>
+#include <openssl/obj_mac.h>
+#include <openssl/ripemd.h>
+#include <openssl/sha.h>
+
+static const char* self;
+static int testnet;
+
+static BN_CTX* bn;
+static EVP_MD_CTX* evp_md;
+
+static void help(void) {
+	printf(
+		"Usage: %s [options]\n"
+		"\n"
+		"Generates Bitcoin version 1 addresses.\n"
+		"\n"
+		"Options:\n"
+		"	-t	Generate a testnet address.\n"
+		"	-h	Show this help.\n",
+		self
+	);
+}
+
+/* Converts a byte array to the Base58Check representation. */
+static void base58check(const void* data, size_t size, char* buffer) {
+	static const char base58check[58] =
+		"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+	size_t index1, index2;
+
+	/* Initialize BIGNUMs for the division algorithm. */
+	BIGNUM* number = BN_bin2bn(data, size, NULL);
+	BIGNUM* base = NULL;
+	BIGNUM* rem = BN_new();
+	BIGNUM* div = BN_new();
+	BN_dec2bn(&base, "58");
+
+	/* Convert the number using the static conversion table. */
+	index1 = 0;
+	do {
+		unsigned int value;
+		BN_div(div, rem, number, base, bn);
+		BN_bn2binpad(rem, (unsigned char*)&value, sizeof value);
+		BN_copy(number, div);
+		buffer[index1++] = base58check[ntohl(value)];
+	}
+	while (!BN_is_zero(number));
+
+	/* Append zeroes for every trailing null byte in the data buffer. */
+	for (index2 = 0; index2 < size && !((char*)data)[index2]; ++index2)
+		buffer[index1++] = base58check[0];
+
+	/* Reverse the string. */
+	for (index2 = 0; index2 < index1 >> 1; ++index2) {
+		char temp = buffer[index2];
+		buffer[index2] = buffer[index1 - index2 - 1];
+		buffer[index1 - index2 - 1] = temp;
+	}
+	buffer[index1] = 0;
+
+	/* Free BIGNUMs. */
+	BN_free(number);
+	BN_free(base);
+	BN_free(rem);
+	BN_free(div);
+}
+
+int main(int argc, char* argv[]) {
+	int option;
+	EC_KEY* key;
+
+	self = *argv;
+	while ((option = getopt(argc, argv, "ht")) != -1)
+		switch (option) {
+		case 't':
+			testnet = 1;
+			break;
+		case 'h':
+			help();
+			return EXIT_SUCCESS;
+		default:
+			fprintf(stderr, "Run `%s -h' for help.\n", self);
+			return EXIT_FAILURE;
+		}
+
+	/* Initialize OpenSSL stuff. */
+	evp_md = EVP_MD_CTX_new();
+	bn = BN_CTX_new();
+
+	/* Generate a new ECDSA secp256k1 key. */
+	key = EC_KEY_new_by_curve_name(NID_secp256k1);
+	EC_KEY_generate_key(key);
+
+	/* Process private key. */
+	{
+		unsigned char buffer1[37];
+		unsigned char buffer2[52];
+
+		/* Get private key. */
+		EC_KEY_priv2oct(key, buffer1 + 1, 32);
+
+		/* Prepend version prefix. */
+		buffer1[0] = !testnet ? 0x80 : 0xEF;
+
+		/* Calculate SHA-256 twice. */
+		EVP_DigestInit(evp_md, EVP_sha256());
+		EVP_DigestUpdate(evp_md, buffer1, 33);
+		EVP_DigestFinal_ex(evp_md, buffer2, NULL);
+		EVP_DigestInit(evp_md, EVP_sha256());
+		EVP_DigestUpdate(evp_md, buffer2, 32);
+		EVP_DigestFinal_ex(evp_md, buffer2, NULL);
+
+		/* Append checksum. */
+		memcpy(buffer1 + 33, buffer2, 4);
+
+		/* Convert to Base58Check. */
+		base58check(buffer1, 37, (char*)buffer2);
+		printf("%s\n", buffer2);
+	}
+
+	/* Process public key. */
+	{
+		unsigned char buffer1[65];
+		unsigned char buffer2[32];
+
+		/* Get public key. */
+		const EC_GROUP* group = EC_KEY_get0_group(key);
+		const EC_POINT* point = EC_KEY_get0_public_key(key);
+		EC_POINT_point2oct(group, point, POINT_CONVERSION_UNCOMPRESSED,
+			buffer1, 65, bn);
+
+		/* Calculate SHA-256. */
+		EVP_DigestInit(evp_md, EVP_sha256());
+		EVP_DigestUpdate(evp_md, buffer1, 65);
+		EVP_DigestFinal_ex(evp_md, buffer2, NULL);
+
+		/* Calculate RIPEMD-160. */
+		EVP_DigestInit(evp_md, EVP_ripemd160());
+		EVP_DigestUpdate(evp_md, buffer2, 32);
+		EVP_DigestFinal_ex(evp_md, buffer2 + 1, NULL);
+
+		/* Prepend version information. */
+		buffer2[0] = !testnet ? 0 : 0x6F;
+
+		/* Calculate SHA-256 twice. */
+		EVP_DigestInit(evp_md, EVP_sha256());
+		EVP_DigestUpdate(evp_md, buffer2, 21);
+		EVP_DigestFinal_ex(evp_md, buffer1, NULL);
+		EVP_DigestInit(evp_md, EVP_sha256());
+		EVP_DigestUpdate(evp_md, buffer1, 32);
+		EVP_DigestFinal_ex(evp_md, buffer1, NULL);
+
+		/* Append checksum. */
+		memcpy(buffer2 + 21, buffer1, 4);
+
+		/* Convert to Base58Check. */
+		base58check(buffer2, 25, (char*)buffer1);
+		printf("%s\n", buffer1);
+	}
+	return EXIT_SUCCESS;
+}
